845596732af677e5108bb54f0879dd5edaa93c78
Cintia <cintia.bsza@gmail.com>
2024-08-30 21:34:31 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "standalone",
+  	reactStrictMode: true,
+  	experimental: {
+    	outputFileTracingRoot: path.join(__dirname, "../../"),
+    	esmExternals: false,
+  },
+};
+};
 
 export default nextConfig;
